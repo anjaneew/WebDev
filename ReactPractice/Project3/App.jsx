cc89805cea62e51bd67d137cb1240749f1d0331e
Anjanee Wijewardana <anjaneew@gmail.com>
2025-08-01 09:15:41 +0000

    CSS methods in React

--- ReactPractice/Project3/App.jsx
+++ ReactPractice/Project3/App.jsx
@@ -3,7 +3,7 @@
 //  1 External  2 Modules 3 Inline
 
 import Button1 from './Button1.jsx'
-import Button2 from './Button/Button2.jsx'
+import Button2 from './Button2/Button2.jsx'
 import Button3 from './Button3.jsx'
 
 
---
+++ ReactPractice/Project3/Button2/Button2.jsx
@@ -0,0 +1,13 @@
+import styles from './Button.module.css'
+
+function Button2(){
+
+    return(
+        <>
+        <button className={styles.button2}>Click me</button>
+        </>
+
+    );
+}
+
+export default Button2
