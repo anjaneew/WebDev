9fb3c8ced465be465397795986aceea380fbc79b
Anjanee Wijewardana <anjaneew@gmail.com>
2025-08-18 04:57:58 +0000

    Generics

--- TypeScript/dist/12Generics.js
+++ TypeScript/dist/12Generics.js
@@ -13,9 +13,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 //---------------------------Generics Intro------------------------------------------------
 //Generic function - returns input value as it is
-function makePens(val) {
+function makePens(data) {
     //type parameter T
-    return val;
+    return data;
 }
 const pen1 = makePens(100);
 const pen2 = makePens("Ronald");
@@ -35,8 +35,15 @@ console.log(`Same Function but with typesafe data that can change:
     4. Number Array: 1,2,3,4
 */
 console.log("\n Arrow Function with Generics \n");
+function displayDataInfo(data) {
+    return `The value is ${data} and it is a data of ${typeof data} type`;
+}
+console.log(displayDataInfo(100));
+console.log(displayDataInfo("Claude is great"));
+console.log(displayDataInfo(true));
+console.log(displayDataInfo([1, 2, 3, 5, 7]));
 //---------------------Arrow Function with Generics---------------------------------------
-const makePaper = (val) => val;
+const makePaper = (data) => data;
 console.log(makePaper(80));
 console.log(makePaper("handmade"));
 console.log("\n Generics Multiple Types \n");
@@ -46,8 +53,37 @@ function rainbowDisplay(numColor, color) {
     return `\nThe number of colors are ${numColor} and colors are ${color}`;
 }
 console.log(rainbowDisplay(2, "purple"));
-console.log("lets see if it works");
-//---------------------------------Generics----------------------------------------------
-//----------------------------------Generics------------------------------------------------
+//-----------------------------Generics Classes--------------------------------------------
+//create flexible and reusable class structures 
+class Report {
+    data;
+    constructor(data) {
+        this.data = data;
+    }
+    showData(msg) {
+        console.log(`${msg} - ${this.data}`);
+    }
+}
+//instance with string
+let report1 = new Report("Dengue Eradication");
+console.log(report1.data);
+report1.showData("The statistics in Sri Lanka");
+//instance with string or number
+let report2 = new Report("2005");
+console.log(report2.data);
+report2.showData("The statistics in India");
+class Collection {
+    data = [];
+    addItem(item) {
+        this.data.push(item);
+    }
+}
+let itemOne = new Collection();
+itemOne.addItem({ itemType: "Book", title: "The Stranger", isbn: 1505832 });
+itemOne.addItem({ itemType: "Book", title: "Moby Dick", isbn: 1505232 });
+console.log(itemOne);
+let itemTwo = new Collection();
+itemTwo.addItem({ itemType: "Game", title: "Age of Mythology", price: 15.28 });
+console.log(itemTwo);
 //----------------------------------Generics------------------------------------------------
 //# sourceMappingURL=12Generics.js.map
--- TypeScript/src/12Generics.ts
+++ TypeScript/src/12Generics.ts
@@ -13,9 +13,9 @@
 //---------------------------Generics Intro------------------------------------------------
 
 //Generic function - returns input value as it is
-function makePens<T>(val: T): T {
+function makePens<T>(data: T): T {
                 //type parameter T
-    return val;
+    return data;
 }
 
 const pen1: number = makePens<number>(100);
@@ -39,9 +39,19 @@ console.log(`Same Function but with typesafe data that can change:
 */
 
 console.log("\n Arrow Function with Generics \n"); 
+
+function displayDataInfo<T>(data: T): string{
+    return `The value is ${data} and it is a data of ${typeof data} type`;
+}
+
+console.log(displayDataInfo(100));
+console.log(displayDataInfo("Claude is great"));
+console.log(displayDataInfo(true));
+console.log(displayDataInfo([1,2,3,5,7]));
+
 //---------------------Arrow Function with Generics---------------------------------------
 
-const makePaper = <T>(val: T): T => val;
+const makePaper = <T>(data: T): T => data;
 
 console.log(makePaper<number>(80));
 console.log(makePaper<string>("handmade"));
@@ -58,10 +68,124 @@ function rainbowDisplay<T, S>(numColor: T, color: S): string {
 console.log(rainbowDisplay(2, "purple"));
 
 
-//---------------------------------Generics----------------------------------------------
+//-----------------------------Generics Classes--------------------------------------------
+//create flexible and reusable class structures 
+
+class Report<T = string>{
+    constructor(public data: T){
+
+    }
+
+    showData(msg: T): void {
+        console.log(`${msg} - ${this.data}`);
+    }
+}
+
+//instance with string
+let report1 = new Report<string>("Dengue Eradication");
+console.log(report1.data);
+report1.showData("The statistics in Sri Lanka");
+
+//instance with string or number
+let report2 = new Report<string | number>("2005");
+console.log(report2.data);
+report2.showData("The statistics in India");
+
+//-------------------------Generics And Interfaces--------------------------------------
+//create reusable and type-safe data structures that can work with different types, 
+//enhancing code flexibility and maintainability.
+
+interface Book{
+    itemType: string;
+    title: string;
+    isbn: number;
+}
+
+interface Game{
+    itemType: string;
+    title: string;
+    price: number;
+}
+
+class Collection<T>{
+    public data: T[] = [];
+
+    addItem(item: T): void {
+        this.data.push(item);
+    }
+}
+
+let itemOne = new Collection<Book>();
+itemOne.addItem({itemType: "Book", title: "The Stranger", isbn: 1505832});
+itemOne.addItem({itemType: "Book", title: "Moby Dick", isbn: 1505232});
+console.log(itemOne);
+
+let itemTwo = new Collection<Game>();
+itemTwo.addItem({itemType: "Game", title: "Age of Mythology", price: 15.28});
+console.log(itemTwo);
+
+//------------------------Exercises--------------------------------------
+//-----------------------Generics Intro Exercises-------------------------
+
+/**1 Create a generic function called `processItem` that:
+* Takes a parameter `item` of type T
+* Returns the same item unchanged
+* Test it with: number 42, string "TypeScript", boolean false, and array [10, 20, 30]*/
+
+/**2 Create a generic function called `describeValue` that:
+* Takes a parameter `value` of type T  
+* Returns a string in format: "Value: [value] has type: [typeof value]"
+* Test with: number 250, string "Learning", boolean true, array [5, 10, 15]*/
+
+/**3 Create an arrow function called `wrapItem` using generic syntax that:
+* Takes a parameter `content` of type T
+* Returns the same content
+* Test with: number 77, string "Arrow Function"*/
 
 
 
-//----------------------------------Generics------------------------------------------------
+//---------------------------Generics Multiple Types Exercises---------------------
 
-//----------------------------------Generics------------------------------------------------
+/**4 Create a generic function called `combineItems` that:
+* Takes two parameters: `first` of type T and `second` of type U
+* Returns a string: "First item: [first], Second item: [second]" 
+* Test with: string "Apple" and number 5, boolean true and string "Complete"*/
+
+/**5 Create a generic function called `compareValues` that:
+* Takes two parameters: `valueA` of type T and `valueB` of type U
+* Returns a string: "Comparing [valueA] (type: [typeof valueA]) with [valueB] (type: [typeof valueB])"
+* Test with: number 100 and string "hundred", string "active" and boolean true*/
+
+//---------------------------Generics Classes Exercises---------------
+
+/**6 Create a generic class called `Storage` with default type string that:
+* Has a constructor taking a parameter `content` of type T and assigns it to public property `content`
+* Has a method `display` that takes parameter `label` of type T and logs: "[label]: [content]"
+* Create instance with string "Database Records" and call display with "Current Data"
+* Create instance with number|string type, value 2024, and call display with "Year"*/
+
+/**7 Create a generic class called `Container` with default type number that:
+* Has a constructor taking parameter `item` of type T and assigns it to public property `item` 
+* Has a method `showInfo` that takes parameter `description` of type T and logs: "[description] -> [item]"
+* Create instance with string "Configuration File" and call showInfo with "System Config"
+* Create instance with boolean|number type, value false, and call showInfo with "Status Check"*/
+
+//---------------------------Generics And Interfaces Exercises-------------
+
+/**8 Define an interface `Vehicle` with properties:
+* itemType: string
+* brand: string  
+* model: string
+* year: number*/
+
+/**9 Define an interface `Electronics` with properties:
+* itemType: string
+* name: string
+* warranty: number*/
+
+/**10 Create a generic class called `Inventory` that:
+* Has a public property `items` initialized as empty array of type T
+* Has method `addProduct` that takes parameter `product` of type T and pushes it to items array
+* Create instance for Vehicle type and add: {itemType: "Vehicle", brand: "Toyota", model: "Camry", year: 2023}
+* Create instance for Electronics type and add: {itemType: "Electronics", name: "Smartphone", warranty: 24}
+* Log both instances to see the results*/
